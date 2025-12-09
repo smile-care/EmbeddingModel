2f9bd416fe3648f92764bfe4b435020b99c58deb
smile-care <1085077389@qq.com>
2025-12-09 23:26:34 +0800

    fiftyone

--- zzz.py
+++ zzz.py
@@ -1,43 +1,49 @@
+import glob
 import json
 import os
+import os.path as osp
 import shutil
 from collections import defaultdict
 from pathlib import Path
 
+import fiftyone as fo
 from PIL import Image
 from tqdm import tqdm
 
 if __name__ == "__main__":
-    save_dir = "data/zhenyu_data/patches/supcon_labels"
-    json_data = json.load(open("data/zhenyu_data/metadata/supcon_dataset.json", 'r', encoding='utf-8'))
+    image_dir = "data/zhenyu_data/patches/supcon_labels/顶盖正面"
     
-    total_patches = json_data['total_patches']
-    patches = json_data['patches']
+    image_list = list(Path(image_dir).glob("*/*.png"))
+    print(f"Found {len(image_list)} images in {image_dir}")
     
+    annotations = {}
+    for img_path in tqdm(image_list):
+        img_path = str(img_path)
+        if img_path.endswith("_mask.png"):
+            continue
+        img_path = Path(img_path).absolute()
+        mask_path = img_path.with_name(img_path.stem + "_mask.png")
+        if not mask_path.exists():
+            print(f"Mask not found for image: {img_path}")
+            continue
+        label = img_path.parent.name
+        annotations[str(img_path)] = [label, mask_path]
     
-    view2paths = defaultdict(list)
-    for patch in tqdm(patches):
-        label = patch['label']
-        view = label.split('_')[0]
-        # patch_path = patch['patch_path']
-        # patch_mask_path = patch['patch_mask_path']
-        
-        # save_path_dir = Path(save_dir) / view / label
-        # save_path_dir.mkdir(parents=True, exist_ok=True)
-        # save_patch_path = save_path_dir / Path(patch_path).name
-        # save_patch_mask_path = save_path_dir / Path(patch_mask_path).name
-        # shutil.copy(patch_path, save_patch_path)
-        # shutil.copy(patch_mask_path, save_patch_mask_path)
-        
-        view2paths[view].append(patch)
+    print(f"Total annotations: {len(annotations)}")
     
+    # Create samples for your data
+    samples = []
+    for filepath, (label, mask_path) in annotations.items():
+        sample = fo.Sample(filepath=filepath)
+
+        # Store segmentation in a field name of your choice
+        sample["ground_truth"] = fo.Classification(label=label)
+        sample["segmentation1"] = fo.Segmentation(filepath=str(mask_path))
+
+        samples.append(sample)
+
+    # Create dataset
+    dataset = fo.Dataset("my-dataset")
+    dataset.add_samples(samples)
     
-    for view, patches in view2paths.items():
-        save_json_path = Path(save_dir) / view / "metadata.json"
-        save_json_path.parent.mkdir(parents=True, exist_ok=True)
-        view_json_data = {
-            'total_patches': len(patches),
-            'patches': patches
-        }
-        with open(save_json_path, 'w', encoding='utf-8') as f:
-            json.dump(view_json_data, f, ensure_ascii=False, indent=4)
+    session = fo.launch_app(dataset)
